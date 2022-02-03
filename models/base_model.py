61eed75ee36f7564ca79c9897cbac576e1d6e383
tsims115 <tsims115@yahoo.com>
2022-02-03 09:52:04 -0800

    Adding to_dict and docs

--- models/base_model.py
+++ models/base_model.py
@@ -9,19 +9,35 @@ import uuid
 class BaseModel:
     """Class BaseModel"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Instanciates with current time and unique ID"""
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    v = datetime.fromisoformat(v)
+                if k == '__class__':
+                    continue
+                setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
+        """Saves new time"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        self.__dict__['__class__'] = BaseModel.__name__
-        self.created_at = datetime.isoformat(self.created_at)
-        self.updated_at = datetime.isoformat(self.updated_at)
-        return self.__dict__
+        """Creates dictionary with key/values of __dict__ of
+        instance"""
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            new_dict[key] = value
+        new_dict['__class__'] = type(self).__name__
+        new_dict['created_at'] = datetime.isoformat(self.created_at)
+        new_dict['updated_at'] = datetime.isoformat(self.updated_at)
+        return new_dict
 
     def __str__(self):
-        return "[{}] ({}) {}".format(BaseModel.__name__, self.id, self.__dict__)
+        """Return string format of Object"""
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
