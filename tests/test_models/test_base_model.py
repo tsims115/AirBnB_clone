dd396745ec086f2b06ffe640ce5f46c75278ca8a
Kaylee Dao <kayleed2@gmail.com>
2022-02-03 16:13:37 -0700

    start of testing base models file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unit testing for BaseModel"""
+
+import unittest
+from models.base_model import BaseModel
+import pep8
+
+
+class TestBaseClass(unittest.TestCase):
+    """ Class to test BaseModel """
+    def setUp(self):
+        """Set up"""
+
+    def tearDown(self):
+        """Tear down"""
+        
