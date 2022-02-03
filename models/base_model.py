2660c17f39fcfd983af193021b972051486f2b2d
tsims115 <tsims115@yahoo.com>
2022-02-03 14:42:18 -0800

    Adding filestorage

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
@@ -22,10 +23,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+        storage.new(self)
 
     def save(self):
         """Saves new time"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Creates dictionary with key/values of __dict__ of
---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module containing class FileStorage"""
+
+
+from models.base_model import BaseModel
+import json
+import os
+
+class FileStorage:
+    """Serializes instances to a
+    JSON file and deserializes JSON file to instances"""
+    def __init__(self):
+        self.__file_path = 'file.json'
+        self.__objects = {}
+    
+    def all(self):
+        """Returns dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key"""
+        self.__objects[type(obj).__name__.id] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        json_string = json.dumps(self.__objects)
+        with open(self.__file_path, 'a+') as f:
+            f.seek(0)
+            f.write(json_string)
+            f.truncate()
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        if os.path.exists(self.__file_path):
+            self.__objects = json.load(f)
