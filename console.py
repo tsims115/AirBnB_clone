40e62701a2f9376f941e7626dc930fa89ef9a8dd
tsims115 <tsims115@yahoo.com>
2022-02-05 18:59:23 -0800

    Adding changes

--- console.py
+++ console.py
@@ -50,12 +50,89 @@ class HBNBCommand(cmd.Cmd):
             return
         string = arg[0] + "." + arg[1]
         all_objs = storage.all()
-        for key in all_objs.keys():
-            if key == string:
-                print(all_objs[key])
-                return
+        if string in all_objs.keys():
+            print(all_objs[string])
+            return
         print("** no instance found **")
+
+    def do_destroy(self, *args):
+        """Deletes an instance based on the class name and id"""
+        if args[0] == '':
+            print("** class name missing **")
+            return
+        string = args[0]
+        arg = string.split(' ')
+        if arg[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(arg) == 1:
+            print("** instance id missing **")
+            return
+        string = arg[0] + "." + arg[1]
+        all_objs = storage.all()
+        if string in all_objs.keys():
+            del all_objs[string]
+            storage.save()
+            return
+
+    def do_all(self, *args):
+        """Prints all string representation of
+        all instances based or not on the class name"""
+        all_objs = storage.all()
+        list_objs = []
+        if args[0] == '':
+            for k, v in all_objs.items():
+                list_objs.append(str(v))
+            print(list_objs)
+            return
+        if args[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        for k, v in all_objs.items():
+            if args[0] == type(v).__name__:
+                list_objs.append(str(v))
+        print(list_objs)
+
+    def do_update(self, *args):
+        """Updates an instance based on the class
+        name and id"""
+        all_objs = storage.all()
+        if args[0] == '':
+            print("** class name missing **")
+            return
+        string = args[0]
+        arg = string.split(' ')
+        if arg[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(arg) == 1:
+            print("** instance id missing **")
+            return
+        string = arg[0] + "." + arg[1]
+        if string not in all_objs.keys():
+            print("** no instance found **")
+            return
+        if len(arg) == 2:
+            print("** attribute name missing **")
+            return
+        if len(arg) == 3:
+            print("** value missing **")
+            return
+        obj = all_objs[string]
+        try:
+            a = float(arg[3])
+            if a.is_integer():
+                a = int(a)
+        except (TypeError, ValueError):
+            """setattr(obj, arg[2], str(arg[3]))"""
+            setattr(all_objs[string], arg[2], str(arg[3]))
+            storage.save()
+            return
+        """setattr(obj, arg[2], a)"""
+        setattr(all_objs[string], arg[2], a)
+        storage.save()
+        return
 
 
 if __name__ == '__main__':
-        HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
