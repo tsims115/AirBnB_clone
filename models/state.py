28b1744c94c4216c381f9faf24dfccb20352e5be
tsims115 <tsims115@yahoo.com>
2022-02-05 22:04:56 -0800

    Adding the rest of the classes

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module models.state with class State"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class State inherits from BaseModel"""
+    name = ""
