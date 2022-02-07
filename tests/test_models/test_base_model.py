9f3a34f670f911fa1b6cec2591a55764963fafeb
tsims115 <tsims115@yahoo.com>
2022-02-07 10:11:56 -0800

    Adding tests for base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,15 +4,18 @@
 import unittest
 from models.base_model import BaseModel
 import pep8
+from datetime import datetime
 
 
 class TestBaseClass(unittest.TestCase):
     """ Class to test BaseModel """
     def setUp(self):
         """Set up"""
+        self.ex1 = BaseModel()
 
     def tearDown(self):
         """Tear down"""
+        del self.ex1
 
     def test_pep8(self):
         """
@@ -31,6 +34,37 @@ class TestBaseClass(unittest.TestCase):
         self.assertTrue(len(BaseModel.__doc__) >= 1)
         # Method docstrings
         self.assertTrue(len(BaseModel.__init__.__doc__) >= 1)
+
+    def test_base_model_uuid(self):
+        """Testing BaseModel UUID"""
+        id = self.ex1.id
+        self.assertEqual(id, self.ex1.id)
+        self.assertEqual(type(id), str)
+
+    def test_base_model_to_dict(self):
+        """Testing to_dict method"""
+        name = self.ex1.to_dict()["__class__"]
+        self.assertEqual(name, "BaseModel")
+        typ = type(self.ex1.updated_at)
+        self.assertEqual(datetime, typ)
+        typ = type(self.ex1.to_dict()["updated_at"])
+        self.assertEqual(typ, str)
+        typ = type(self.ex1.created_at)
+        self.assertEqual(datetime, typ)
+        typ = type(self.ex1.to_dict()["created_at"])
+        self.assertEqual(typ, str)
+        typ = type(self.ex1.to_dict())
+        self.assertEqual(dict, typ)
+
+    def test_base_model_str(self):
+        """Testing __str__ method"""
+        self.assertEqual(type(str(self.ex1)), str)
+
+    def test_save_method(self):
+        """Testing save method for time"""
+        time = self.ex1.updated_at
+        self.ex1.save()
+        self.assertFalse(time == self.ex1.updated_at)
 
 if __name__ == "__main__":
     unittest.main()
