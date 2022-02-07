38ee836b5f5230c668feecaa66d6910e8197255e
Kaylee Dao <kayleed2@gmail.com>
2022-02-07 17:52:32 -0600

    Basic user tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,6 +2,7 @@
 """Unit testing for User"""
 
 from re import U
+import threading
 import unittest
 from models.user import User
 import pep8
@@ -11,9 +12,11 @@ class TestUserClass(unittest.TestCase):
     """ Class to test User """
     def setUp(self):
         """Set up"""
+        self.thing = User()
 
     def tearDown(self):
         """Tear down"""
+        del self.thing
 
     def test_pep8(self):
         """
@@ -32,6 +35,17 @@ class TestUserClass(unittest.TestCase):
         self.assertTrue(len(User.__doc__) >= 1)
         # Method docstrings
         self.assertTrue(len(User.__init__.__doc__) >= 1)
+
+    def test_user_attributes(self):
+        """Tests attributes of User"""
+        self.thing.email = "example@ex.com"
+        self.assertEqual(self.thing.email, "example@ex.com")
+        self.thing.password = "thisispassword"
+        self.assertEqual(self.thing.password, "thisispassword")
+        self.thing.first_name = "Kaylee"
+        self.assertEqual(self.thing.first_name, "Kaylee")
+        self.thing.last_name = "LastName"
+        self.assertEqual(self.thing.last_name, "LastName")
 
 if __name__ == "__main__":
     unittest.main()
