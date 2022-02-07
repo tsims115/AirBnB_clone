5b8cee6fc95e4a97fde046421aa1588bb65c2e4d
tsims115 <tsims115@yahoo.com>
2022-02-07 12:29:35 -0800

    Adding new file_storage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,15 +4,24 @@
 import unittest
 from models.engine.file_storage import FileStorage
 import pep8
+import json
+import sys
+from models.base_model import BaseModel
+from models import storage
 
 
 class TestFileStorageClass(unittest.TestCase):
     """ Class to test FileStorage """
     def setUp(self):
         """Set up"""
+        self.ex1 = FileStorage()
+        self.model = BaseModel()
+        storage.save()
 
     def tearDown(self):
         """Tear down"""
+        del self.ex1
+        del self.model
 
     def test_pep8(self):
         """
@@ -31,6 +40,18 @@ class TestFileStorageClass(unittest.TestCase):
         self.assertTrue(len(FileStorage.__doc__) >= 1)
         # Method docstrings
         self.assertTrue(len(FileStorage.__init__.__doc__) >= 1)
+
+    def test_file_storage_reload_all(self):
+        """Tests reload method"""
+        self.assertEqual(self.ex1.all(), {})
+        self.ex1.reload()
+        with open("file.json", 'r') as f:
+            data = json.load(f)
+        if data == {}:
+            self.assertEqual(data, self.ex1.all())
+        self.assertTrue(data.keys() == self.ex1.all().keys())
+        num = "BaseModel" + "." + self.model.id
+        self.assertTrue(num in data.keys())
 
 if __name__ == "__main__":
     unittest.main()
